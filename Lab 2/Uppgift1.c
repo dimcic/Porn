5d52f1bba2754d7a04c5d455c135104bb9b023cd
dimcic <44384760+dimcic@users.noreply.github.com>
2018-11-06 06:57:07 +0100

    Update Uppgift1.c
    JAG fixade den noob 45k for what

--- Lab 2/Uppgift1.c
+++ Lab 2/Uppgift1.c
@@ -44,3 +44,88 @@ int main()
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <time.h>  
+
+
+int main()
+{
+	srand((int)time(NULL));
+	system("chcp 1252");
+	system("cls");
+	
+	int antalTal = 0;
+	int antalLoops = 0;
+	int summa = 0;
+	int belopp = 0;
+	int medelvarde = 0;
+	antalTal = rand() % 10;
+
+
+	printf("Ange beloppet vilket summan skall understiga: \n");
+	scanf_s("%d", &belopp);
+
+	while (summa < belopp)
+	{
+		int random = rand() % belopp;
+
+		
+		//* Innan vi adderar summan och det slumpmässiga talet så vill vi se till så att det inte överstiger beloppet.
+		if (summa + random >= belopp)
+		{
+			break;
+		}
+
+		summa = summa + random;
+		antalTal = antalTal + 1;
+	}
+	
+	printf("Antal tal som summerats: %d \n", antalTal);
+	printf("Summa: %d\n", summa);
+	
+	// Om användaren skriver in ett belopp som är mindre än det intervall som vi genererar tal för så kan vi
+	// ha avbrutit while-loopen innan vi summerat några tal.
+	if (summa > 0)
+	{
+		printf("Medelvärde: %.1f\n", summa / (float)antalTal);
+	}
+	else
+	{
+		printf("Inget medelvärde eftersom att antal tal var 0.\n");
+	}
+
+	system("pause");
+
+	return 0;
+}
